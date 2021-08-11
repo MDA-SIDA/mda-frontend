c42976d2a77879c792a1dbd8eb01ceb1e519dbc4
eduana <eduana@tframe.de>
2021-08-11 11:19:54 +0200

    input improvements

--- src/common/Input/index.jsx
+++ src/common/Input/index.jsx
@@ -1,13 +1,60 @@
 import React from "react";
 import {Field} from "formik";
-import styles from "./index.module.scss";
+import "./index.scss";
 
-function Input({id, name, label, placeholder, touched, type = "text", error}) {
+function Input({
+	id,
+	name,
+	label,
+	placeholder,
+	touched,
+	type = "text",
+	error,
+	containerStyle,
+	inputStyle,
+	as = "input",
+	value,
+	withOnChange,
+	onChange,
+}) {
 	return (
-		<div className={styles.input_container}>
+		<div className={`input_container ${containerStyle}`}>
 			<label htmlFor={name}>{label}</label>
-			<Field id={id} name={name} placeholder={placeholder} type={type} />
-			{touched && error && <div className={styles.input_container__error}>{error}</div>}
+			{!withOnChange && (
+				<Field
+					id={id}
+					name={name}
+					placeholder={placeholder}
+					type={type}
+					className={inputStyle}
+					as={as}
+					value={value}
+					autoComplete="off"
+				/>
+			)}
+			{withOnChange && (
+				<Field name={name}>
+					{({field}) => (
+						<input
+							{...field}
+							id={id}
+							name={name}
+							type={type}
+							onChange={(value) => {
+								// formik's onChange
+								field.onChange(value);
+								// our custom onChange
+								onChange(value);
+							}}
+							className={inputStyle}
+							placeholder={placeholder}
+							value={value}
+							autoComplete="off"
+						/>
+					)}
+				</Field>
+			)}
+			{touched && error && <div className="input_container__error">{error}</div>}
 		</div>
 	);
 }
