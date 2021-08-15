a718ec268bb738908c86f60750fdee24fe84991c
Lorjana Pacarizi <loriana.pacarizi@gmail.com>
2021-08-15 14:27:43 +0200

    code formated

--- src/common/Select/Option/index.jsx
+++ src/common/Select/Option/index.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import { components } from 'react-select'
-import './index.scss'
+import React from "react";
+import {components} from "react-select";
+import "./index.scss";
 
-const Option = ({ isCountry, isSelected, value, label, isMulti, ...props }) => (
-  <div>
-    <components.Option {...props}>
-      <div className="option">
-        <label>{label}</label>
-        {isMulti && (
-          <input type="checkbox" checked={isSelected} onChange={() => {}} />
-        )}
-      </div>
-    </components.Option>
-  </div>
-)
+const Option = ({isCountry, isSelected, value, label, isMulti, ...props}) => (
+	<div>
+		<components.Option {...props}>
+			<div className="option">
+				<label>{label}</label>
+				{isMulti && <input type="checkbox" checked={isSelected} onChange={() => {}} />}
+			</div>
+		</components.Option>
+	</div>
+);
 
-export default Option
+export default Option;
