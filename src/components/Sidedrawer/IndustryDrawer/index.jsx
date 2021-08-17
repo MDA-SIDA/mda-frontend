11a3d78e24cc4f6b4b1c3c6c19d35945978b742d
Eduanath <eduana.thaci@studentet.uni-pr.edu>
2021-08-17 22:10:37 +0200

    dynamic industries form

--- src/components/Sidedrawer/IndustryDrawer/index.jsx
+++ src/components/Sidedrawer/IndustryDrawer/index.jsx
@@ -1,22 +1,77 @@
 import React, {useState} from "react";
 import {Formik, Form} from "formik";
 import Input from "@common/Input";
-import * as Yup from "yup";
-import Button from "@common/Button";
+import * as yup from "yup";
 import {withRouter} from "react-router";
 import {connect} from "react-redux";
 import {actions} from "@sagas/profile/index";
 import SideDrawer from "@components/Sidedrawer/index";
 import styles from "./index.module.scss";
 
-const Index = (props) => (
-	<SideDrawer open={props.open} closed={props.closed}>
-		<div className={styles.container}>TESST</div>
-	</SideDrawer>
-);
+// TODO: add this object to a separate file
+const industryInputs = {
+	arbk: {
+		validationSchema: yup.object().shape({
+			companyName: yup.string().label("Company Name").required().nullable(),
+		}),
+		inputs: [
+			{
+				type: "text",
+				placeholder: "Company Name",
+				name: "companyName",
+				label: "Company Name",
+			},
+		],
+	},
+	masht: {
+		validationSchema: yup.object().shape({
+			companyName: yup.string().label("Test").required().nullable(),
+		}),
+		inputs: [
+			{
+				type: "text",
+				placeholder: "Test",
+				name: "test",
+				label: "Test",
+			},
+		],
+	},
+};
+
+const IndustryDrawer = ({open, closed, onSubmit, industry}) => {
+	const {inputs} = industryInputs[industry];
+	const validationSchema = industryInputs[industry]?.validationSchema;
+	return (
+		<SideDrawer open={open} closed={closed}>
+			<div className={styles.container}>
+				<Formik
+					enableReinitialize={true}
+					initialValues={{}}
+					onSubmit={onSubmit}
+					validationSchema={validationSchema}
+				>
+					{({errors, touched, values}) => (
+						<Form>
+							{inputs?.map((input) => (
+								<Input
+									name={input?.name}
+									placeholder={input?.placeholder}
+									error={errors[input?.name]}
+									touched={touched[input?.name]}
+									value={values[input?.name]}
+									label={input?.label}
+								/>
+							))}
+						</Form>
+					)}
+				</Formik>
+			</div>
+		</SideDrawer>
+	);
+};
 
 const mapDispatchToProps = {
 	editProfile: actions.editProfile,
 };
 
-export default connect(null, mapDispatchToProps)(withRouter(Index));
+export default connect(null, mapDispatchToProps)(withRouter(IndustryDrawer));
