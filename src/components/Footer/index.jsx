e92f4b6685b4c04abccf89bd4e1cfd428accc206
Eduanath <eduana.thaci@studentet.uni-pr.edu>
2021-09-05 00:41:29 +0200

    implemented required changes on footer section

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Logo from "@img/svg/logo.svg";
 import SELogo from "@img/svg/se_logo.svg";
 import MDALogo from "@img/svg/mda_logo.svg";
 import "./index.scss";
@@ -7,17 +6,6 @@ import "./index.scss";
 function Footer() {
 	return (
 		<footer>
-			<div className="main">
-				<div className="main_first">
-					<img src={Logo} alt="logo" />
-					<div className="main_first__info">
-						<p className="main_first__info__address">Adresa</p>
-						<p>Bill Clinton Boulevard, 5/8 D, 10000 Pristina,</p>
-						<p> Kosovo.</p>
-					</div>
-				</div>
-				<p className="copyright">© 2021, MDA</p>
-			</div>
 			<div className="collab">
 				<p>Co-founded by:</p>
 				<img src={SELogo} alt="Sweden Logo" />
