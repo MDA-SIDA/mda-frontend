d847a55f00813c9fb2695e7a11559b5f45cb3ca1
Eduanath <eduana.thaci@studentet.uni-pr.edu>
2021-09-11 17:46:55 +0200

    applied changes to the ARBK graphs

--- src/components/Graphs/ARBK.jsx
+++ src/components/Graphs/ARBK.jsx
@@ -3,188 +3,157 @@ import {connect} from "react-redux";
 import {actions} from "@sagas/industries/arbk";
 import Chart from "@common/Chart";
 import {groupBy} from "lodash";
-import ProgressBar from "@common/ProgressBar";
-import {
-	getDatasets,
-	getGjiniaDataset,
-	getGjiniaMesatarja,
-	getStatusiBizneseveDataset,
-} from "./utils";
+import {getDatasets, sortLabels} from "./utils";
 
 function ARBK({
-	fetchNrBizneseve,
-	fetchNrBizneseveKomuna,
-	fetchGjinia,
-	fetchLlojiBiznesit,
-	fetchLlojiBiznesitKomuna,
-	fetchStatusiBizneseve,
-	fetchSektoreBujqesi,
-	nrBizneseve,
-	gjinia,
-	llojiBiznesit,
-	statusiBizneseve,
-	sektoreBujqesi,
+	fetchArbkLlojiBiznesit,
+	fetchArbkViti,
+	fetchArbkSektori,
+	fetchArbkGjinia,
+	fetchArbkStatusi,
+	fetchArbkKomuna,
+	arbkLlojiBiznesit,
+	arbkViti,
+	arbkSektori,
+	arbkGjinia,
+	arbkStatusi,
+	arbkKomuna,
 	filters,
 }) {
 	useEffect(() => {
-		fetchNrBizneseve(filters);
-		fetchNrBizneseveKomuna(filters);
-		fetchGjinia(filters);
-		fetchLlojiBiznesit(filters);
-		fetchLlojiBiznesitKomuna(filters);
-		fetchStatusiBizneseve(filters);
-		fetchSektoreBujqesi(filters);
+		fetchArbkLlojiBiznesit(filters);
+		fetchArbkViti(filters);
+		fetchArbkSektori(filters);
+		fetchArbkGjinia(filters);
+		fetchArbkStatusi(filters);
+		fetchArbkKomuna(filters);
 	}, [
-		fetchNrBizneseve,
-		fetchNrBizneseveKomuna,
-		fetchGjinia,
-		fetchLlojiBiznesit,
-		fetchLlojiBiznesitKomuna,
-		fetchStatusiBizneseve,
-		fetchSektoreBujqesi,
+		fetchArbkLlojiBiznesit,
+		fetchArbkViti,
+		fetchArbkSektori,
+		fetchArbkGjinia,
+		fetchArbkStatusi,
+		fetchArbkKomuna,
 		filters,
 	]);
 
-	const nrBizneseveDataSets = getDatasets({
+	const arbkLlojiBiznesiDataSets = getDatasets({
 		filters,
-		items: nrBizneseve,
+		items: arbkLlojiBiznesit,
 		singleItemLabel: "Numri i bizneseve",
-		property: "nrbizneseve",
-		filterBy: "viti",
-	});
-
-	const sektoreBujqesiDataSets = getDatasets({
-		filters,
-		items: sektoreBujqesi,
-		singleItemLabel: "Numri i sektoreve ne bujqesi",
-		property: "nrpunetoreve",
-		filterBy: "sektori",
-	});
-
-	const llojiBiznesitDataSets = getDatasets({
-		filters,
-		items: llojiBiznesit,
-		singleItemLabel: "Numri i llojit te biznesit",
-		property: "counter",
+		property: "numribizneseve",
 		filterBy: "llojibiznesit",
 	});
 
-	const gjiniaData = getGjiniaDataset({items: gjinia, filters});
-	const statusiBizneseveData = getStatusiBizneseveDataset({items: statusiBizneseve, filters});
+	const arbkVitiDataSets = getDatasets({
+		filters,
+		items: arbkViti,
+		singleItemLabel: "Numri i bizneseve",
+		property: "numribizneseve",
+		filterBy: "viti",
+	});
+
+	const arbkSektoriDataSets = getDatasets({
+		filters,
+		items: arbkSektori,
+		singleItemLabel: "Numri i bizneseve",
+		property: "numribizneseve",
+		filterBy: "sektori",
+	});
+
+	const arbkGjiniaDataSets = getDatasets({
+		filters,
+		items: arbkGjinia,
+		singleItemLabel: "Numri i bizneseve",
+		property: "numribizneseve",
+		filterBy: "gjinia",
+	});
+
+	const arbkStatusiDataSets = getDatasets({
+		filters,
+		items: arbkStatusi,
+		singleItemLabel: "Numri i bizneseve",
+		property: "numribizneseve",
+		filterBy: "statusiibiznesit",
+	});
+
+	const arbkKomunaDataSets = getDatasets({
+		filters,
+		items: arbkStatusi,
+		singleItemLabel: "Numri i bizneseve",
+		property: "numribizneseve",
+		filterBy: "komunaemri",
+	});
 
 	return (
 		<>
 			<Chart
-				title="Sektore Bujqesi"
+				title="Numri i bizneseve sipas llojit"
 				type="bar"
 				data={{
-					labels: Object.keys(groupBy(sektoreBujqesi, "sektori")),
-					datasets: sektoreBujqesiDataSets,
+					labels: sortLabels(Object.keys(groupBy(arbkLlojiBiznesit, "llojibiznesit"))),
+					datasets: arbkLlojiBiznesiDataSets,
 				}}
 			/>
 			<Chart
-				title="Numri Bizneseve"
+				title="Numri i bizneseve sipas vitit"
 				type="line"
 				data={{
-					labels: Object.keys(groupBy(nrBizneseve, "viti")),
-					datasets: nrBizneseveDataSets,
+					labels: sortLabels(Object.keys(groupBy(arbkViti, "viti"))),
+					datasets: arbkVitiDataSets,
 				}}
 			/>
-			{statusiBizneseveData?.map((item, index) => (
-				<Chart
-					key={`${index} item=index`}
-					title={`Statusi i biznesit ${item.label}`}
-					type="pie"
-					data={{
-						labels: ["Aktiv", "Jo aktiv"],
-						datasets: [
-							{
-								label: item.label,
-								data: [item.data?.countaktiv, item.data?.countjoaktiv],
-								backgroundColor: ["#005293", "#BABABA"],
-							},
-						],
-					}}
-					options={{
-						responsive: true,
-						plugins: {
-							legend: {
-								position: "bottom",
-							},
-						},
-					}}
-				/>
-			))}
 			<Chart
-				title="Lloji Biznesit"
+				title="Numri i bizneseve sipas sektoreve"
 				type="bar"
 				data={{
-					labels: Object.keys(groupBy(llojiBiznesit, "llojibiznesit")),
-					datasets: llojiBiznesitDataSets,
+					labels: sortLabels(Object.keys(groupBy(arbkSektori, "sektori"))),
+					datasets: arbkSektoriDataSets,
 				}}
 			/>
-			<div className="card_container">
-				<div
-					className="chart_title"
-					style={{paddingLeft: 0, paddingTop: 5, paddingBottom: 10}}
-				>
-					Total pronarë
-				</div>
-				{gjiniaData?.map((item, index) => (
-					<div className="card_item" key={`${index} item`}>
-						<div className="card_item_femra">
-							<ProgressBar
-								bgcolor="#ddb40a"
-								completed={getGjiniaMesatarja(
-									item?.data?.totalfemra,
-									item?.data?.maxfemra,
-								)?.toFixed()}
-							/>
-						</div>
-						<div className="card_item_place">{item?.label}</div>
-						<div className="card_item_meshkuj">
-							<ProgressBar
-								bgcolor="#00517D"
-								completed={getGjiniaMesatarja(
-									item?.data?.totalmeshkuj,
-									item?.data?.maxmeshkuj,
-								)?.toFixed()}
-							/>
-						</div>
-					</div>
-				))}
-				<div className="legend">
-					<div className="legend_item">
-						<div className="legend_item__icon" style={{backgroundColor: "#ddb40a"}} />
-						<div className="legend_item__label">Femra</div>
-					</div>
-					<div className="legend_item">
-						<div className="legend_item__icon" style={{backgroundColor: "#00517D"}} />
-						<div className="legend_item__label">Meshkuj</div>
-					</div>
-				</div>
-			</div>
+			<Chart
+				title="Numri i bizneseve sipas gjinise"
+				type="pie"
+				data={{
+					labels: sortLabels(Object.keys(groupBy(arbkGjinia, "gjinia"))),
+					datasets: arbkGjiniaDataSets,
+				}}
+			/>
+			<Chart
+				title="Numri i bizneseve sipas statusit"
+				type="pie"
+				data={{
+					labels: sortLabels(Object.keys(groupBy(arbkStatusi, "statusiibiznesit"))),
+					datasets: arbkStatusiDataSets,
+				}}
+			/>
+			<Chart
+				title="Numri i bizneseve sipas komunave"
+				type="bar"
+				data={{
+					labels: sortLabels(Object.keys(groupBy(arbkKomuna, "komunaemri"))),
+					datasets: arbkKomunaDataSets,
+				}}
+			/>
 		</>
 	);
 }
 
 const mapDispatchToProps = {
-	fetchNrBizneseve: actions.fetchNrBizneseve,
-	fetchNrBizneseveKomuna: actions.fetchNrBizneseveKomuna,
-	fetchGjinia: actions.fetchGjinia,
-	fetchLlojiBiznesit: actions.fetchLlojiBiznesit,
-	fetchLlojiBiznesitKomuna: actions.fetchLlojiBiznesitKomuna,
-	fetchStatusiBizneseve: actions.fetchStatusiBizneseve,
-	fetchSektoreBujqesi: actions.fetchSektoreBujqesi,
+	fetchArbkLlojiBiznesit: actions.fetchArbkLlojiBiznesit,
+	fetchArbkViti: actions.fetchArbkViti,
+	fetchArbkSektori: actions.fetchArbkSektori,
+	fetchArbkGjinia: actions.fetchArbkGjinia,
+	fetchArbkStatusi: actions.fetchArbkStatusi,
+	fetchArbkKomuna: actions.fetchArbkKomuna,
 };
 const mapStateToProps = (state) => ({
-	nrBizneseve: state.app.industries.arbk.nrBizneseve,
-	nrBizneseveKomuna: state.app.industries.arbk.nrBizneseveKomuna,
-	gjinia: state.app.industries.arbk.gjinia,
-	llojiBiznesit: state.app.industries.arbk.llojiBiznesit,
-	llojiBiznesitKomuna: state.app.industries.arbk.llojiBiznesitKomuna,
-	statusiBizneseve: state.app.industries.arbk.statusiBizneseve,
-	sektoreBujqesi: state.app.industries.arbk.sektoreBujqesi,
+	arbkLlojiBiznesit: state.app.industries.arbk.arbkLlojiBiznesit,
+	arbkViti: state.app.industries.arbk.arbkViti,
+	arbkSektori: state.app.industries.arbk.arbkSektori,
+	arbkGjinia: state.app.industries.arbk.arbkGjinia,
+	arbkStatusi: state.app.industries.arbk.arbkStatusi,
+	arbkKomuna: state.app.industries.arbk.arbkKomuna,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ARBK);
--- src/store/sagas/app/industries/arbk.js
+++ src/store/sagas/app/industries/arbk.js
@@ -7,61 +7,54 @@ import axios from "@utils/axios";
 import {getParams} from "../utils";
 
 const PREFIX = "@app/ARBK/index";
-export const FETCH_NR_BIZNESEVE = `${PREFIX}FETCH_NR_BIZNESEVE`;
-export const FETCH_NR_BIZNESEVE_SUCCESS = `${PREFIX}FETCH_NR_BIZNESEVE_SUCCESS`;
-export const FETCH_NR_BIZNESEVE_KOMUNA = `${PREFIX}FETCH_NR_BIZNESEVE_KOMUNA`;
-export const FETCH_NR_BIZNESEVE_KOMUNA_SUCCESS = `${PREFIX}FETCH_NR_BIZNESEVE_KOMUNA_SUCCESS`;
-export const FETCH_GJINIA = `${PREFIX}FETCH_GJINIA`;
-export const FETCH_GJINIA_SUCCESS = `${PREFIX}FETCH_GJINIA_SUCCESS`;
-export const FETCH_LLOJI_BIZNESIT = `${PREFIX}FETCH_LLOJI_BIZNESIT`;
-export const FETCH_LLOJI_BIZNESIT_SUCCESS = `${PREFIX}FETCH_LLOJI_BIZNESIT_SUCCESS`;
-export const FETCH_LLOJI_BIZNESIT_KOMUNA = `${PREFIX}FETCH_LLOJI_BIZNESIT_KOMUNA`;
-export const FETCH_LLOJI_BIZNESIT_KOMUNA_SUCCESS = `${PREFIX}FETCH_LLOJI_BIZNESIT_KOMUNA_SUCCESS`;
-export const FETCH_STATUSI_BIZNESEVE = `${PREFIX}FETCH_STATUSI_BIZNESEVE`;
-export const FETCH_STATUSI_BIZNESEVE_SUCCESS = `${PREFIX}FETCH_STATUSI_BIZNESEVE_SUCCESS`;
-export const FETCH_SEKTORE_BUJQESI = `${PREFIX}FETCH_SEKTORE_BUJQESI`;
-export const FETCH_SEKTORE_BUJQESI_SUCCESS = `${PREFIX}FETCH_SEKTORE_BUJQESI_SUCCESS`;
+export const FETCH_ARBK_LLOJI_BIZNESIT = `${PREFIX}FETCH_ARBK_LLOJI_BIZNESIT`;
+export const FETCH_ARBK_LLOJI_BIZNESIT_SUCCESS = `${PREFIX}FETCH_ARBK_LLOJI_BIZNESIT_SUCCESS`;
+export const FETCH_ARBK_VITI = `${PREFIX}FETCH_ARBK_VITI`;
+export const FETCH_ARBK_VITI_SUCCESS = `${PREFIX}FETCH_ARBK_VITI_SUCCESS`;
+export const FETCH_ARBK_SEKTORI = `${PREFIX}FETCH_ARBK_SEKTORI`;
+export const FETCH_ARBK_SEKTORI_SUCCESS = `${PREFIX}FETCH_ARBK_SEKTORI_SUCCESS`;
+export const FETCH_ARBK_GJINIA = `${PREFIX}FETCH_ARBK_GJINIA`;
+export const FETCH_ARBK_GJINIA_SUCCESS = `${PREFIX}FETCH_ARBK_GJINIA_SUCCESS`;
+export const FETCH_ARBK_STATUSI = `${PREFIX}FETCH_ARBK_STATUSI`;
+export const FETCH_ARBK_STATUSI_SUCCESS = `${PREFIX}FETCH_ARBK_STATUSI_SUCCESS`;
+export const FETCH_ARBK_KOMUNA = `${PREFIX}FETCH_ARBK_KOMUNA`;
+export const FETCH_ARBK_KOMUNA_SUCCESS = `${PREFIX}FETCH_ARBK_KOMUNA_SUCCESS`;
 
 const logger = new Logger("Saga>ARBK>Index");
 const _state = {
-	nrBizneseve: null,
-	nrBizneseveKomuna: null,
-	gjinia: null,
-	llojiBiznesit: null,
-	llojiBiznesitKomuna: null,
-	statusiBizneseve: null,
-	sektoreBujqesi: null,
+	arbkLlojiBiznesit: null,
+	arbkViti: null,
+	arbkSektori: null,
+	arbkGjinia: null,
+	arbkStatusi: null,
+	arbkKomuna: null,
 };
 
 const reducer = (state = _state, action) =>
 	produce(state, (draft) => {
 		switch (action.type) {
-			case FETCH_NR_BIZNESEVE_SUCCESS:
-				draft.nrBizneseve = action.payload;
+			case FETCH_ARBK_LLOJI_BIZNESIT_SUCCESS:
+				draft.arbkLlojiBiznesit = action.payload;
 				break;
 
-			case FETCH_NR_BIZNESEVE_KOMUNA_SUCCESS:
-				draft.nrBizneseveKomuna = action.payload;
+			case FETCH_ARBK_VITI_SUCCESS:
+				draft.arbkViti = action.payload;
 				break;
 
-			case FETCH_GJINIA_SUCCESS:
-				draft.gjinia = action.payload;
+			case FETCH_ARBK_SEKTORI_SUCCESS:
+				draft.arbkSektori = action.payload;
 				break;
 
-			case FETCH_LLOJI_BIZNESIT_SUCCESS:
-				draft.llojiBiznesit = action.payload;
+			case FETCH_ARBK_GJINIA_SUCCESS:
+				draft.arbkGjinia = action.payload;
 				break;
 
-			case FETCH_LLOJI_BIZNESIT_KOMUNA_SUCCESS:
-				draft.llojiBiznesitKomuna = action.payload;
+			case FETCH_ARBK_STATUSI_SUCCESS:
+				draft.arbkStatusi = action.payload;
 				break;
 
-			case FETCH_STATUSI_BIZNESEVE_SUCCESS:
-				draft.statusiBizneseve = action.payload;
-				break;
-
-			case FETCH_SEKTORE_BUJQESI_SUCCESS:
-				draft.sektoreBujqesi = action.payload;
+			case FETCH_ARBK_KOMUNA_SUCCESS:
+				draft.arbkKomuna = action.payload;
 				break;
 
 			default:
@@ -71,113 +64,96 @@ const reducer = (state = _state, action) =>
 export default reducer;
 
 export const actions = {
-	fetchNrBizneseve: (payload) => createAction(FETCH_NR_BIZNESEVE, {payload}),
-	fetchNrBizneseveSuccess: (payload) => createAction(FETCH_NR_BIZNESEVE_SUCCESS, {payload}),
-	fetchNrBizneseveKomuna: (payload) => createAction(FETCH_NR_BIZNESEVE_KOMUNA, {payload}),
-	fetchNrBizneseveKomunaSuccess: (payload) =>
-		createAction(FETCH_NR_BIZNESEVE_KOMUNA_SUCCESS, {payload}),
-	fetchGjinia: (payload) => createAction(FETCH_GJINIA, {payload}),
-	fetchGjiniaSuccess: (payload) => createAction(FETCH_GJINIA_SUCCESS, {payload}),
-	fetchLlojiBiznesit: (payload) => createAction(FETCH_LLOJI_BIZNESIT, {payload}),
-	fetchLlojiBiznesitSuccess: (payload) => createAction(FETCH_LLOJI_BIZNESIT_SUCCESS, {payload}),
-	fetchLlojiBiznesitKomuna: (payload) => createAction(FETCH_LLOJI_BIZNESIT_KOMUNA, {payload}),
-	fetchLlojiBiznesitKomunaSuccess: (payload) =>
-		createAction(FETCH_LLOJI_BIZNESIT_KOMUNA_SUCCESS, {payload}),
-	fetchStatusiBizneseve: (payload) => createAction(FETCH_STATUSI_BIZNESEVE, {payload}),
-	fetchStatusiBizneseveSuccess: (payload) =>
-		createAction(FETCH_STATUSI_BIZNESEVE_SUCCESS, {payload}),
-	fetchSektoreBujqesi: (payload) => createAction(FETCH_SEKTORE_BUJQESI, {payload}),
-	fetchSektoreBujqesiSuccess: (payload) => createAction(FETCH_SEKTORE_BUJQESI_SUCCESS, {payload}),
+	fetchArbkLlojiBiznesit: (payload) => createAction(FETCH_ARBK_LLOJI_BIZNESIT, {payload}),
+	fetchArbkLlojiBiznesitSuccess: (payload) =>
+		createAction(FETCH_ARBK_LLOJI_BIZNESIT_SUCCESS, {payload}),
+	fetchArbkViti: (payload) => createAction(FETCH_ARBK_VITI, {payload}),
+	fetchArbkVitiSuccess: (payload) => createAction(FETCH_ARBK_VITI_SUCCESS, {payload}),
+	fetchArbkSektori: (payload) => createAction(FETCH_ARBK_SEKTORI, {payload}),
+	fetchArbkSektoriSuccess: (payload) => createAction(FETCH_ARBK_SEKTORI_SUCCESS, {payload}),
+	fetchArbkGjinia: (payload) => createAction(FETCH_ARBK_GJINIA, {payload}),
+	fetchArbkGjiniaSuccess: (payload) => createAction(FETCH_ARBK_GJINIA_SUCCESS, {payload}),
+	fetchArbkStatusi: (payload) => createAction(FETCH_ARBK_STATUSI, {payload}),
+	fetchArbkStatusiSuccess: (payload) => createAction(FETCH_ARBK_STATUSI_SUCCESS, {payload}),
+	fetchArbkKomuna: (payload) => createAction(FETCH_ARBK_KOMUNA, {payload}),
+	fetchArbkKomunaSuccess: (payload) => createAction(FETCH_ARBK_KOMUNA_SUCCESS, {payload}),
 };
 
 export const sagas = {
-	*fetchNrBizneseve({payload}) {
+	*fetchArbkLlojiBiznesit({payload}) {
 		try {
 			const {komunaQuery, vendbanimiQuery, regjioniQuery} = getParams(payload);
 			const response = yield axios.get(
 				// eslint-disable-next-line max-len
-				`/industries/?${komunaQuery}${vendbanimiQuery}${regjioniQuery}&type=nrBizneseve`,
+				`/industries/?${komunaQuery}${vendbanimiQuery}${regjioniQuery}&type=arbkLlojiBiznesit`,
 			);
 
-			yield put(actions.fetchNrBizneseveSuccess(response?.data));
+			yield put(actions.fetchArbkLlojiBiznesitSuccess(response?.data));
 		} catch (error) {
 			logger.error(error);
 		}
 	},
-	*fetchNrBizneseveKomuna({payload}) {
+	*fetchArbkViti({payload}) {
 		try {
 			const {komunaQuery, vendbanimiQuery, regjioniQuery} = getParams(payload);
 			const response = yield axios.get(
 				// eslint-disable-next-line max-len
-				`/industries/?${komunaQuery}${vendbanimiQuery}${regjioniQuery}&type=nrBizneseveKomuna`,
+				`/industries/?${komunaQuery}${vendbanimiQuery}${regjioniQuery}&type=arbkViti`,
 			);
 
-			yield put(actions.fetchNrBizneseveKomunaSuccess(response?.data));
+			yield put(actions.fetchArbkVitiSuccess(response?.data));
 		} catch (error) {
 			logger.error(error);
 		}
 	},
-	*fetchGjinia({payload}) {
+	*fetchArbkSektori({payload}) {
 		try {
 			const {komunaQuery, vendbanimiQuery, regjioniQuery} = getParams(payload);
 			const response = yield axios.get(
 				// eslint-disable-next-line max-len
-				`/industries/?${komunaQuery}${vendbanimiQuery}${regjioniQuery}&type=gjinia`,
+				`/industries/?${komunaQuery}${vendbanimiQuery}${regjioniQuery}&type=arbkSektori`,
 			);
 
-			yield put(actions.fetchGjiniaSuccess(response?.data));
+			yield put(actions.fetchArbkSektoriSuccess(response?.data));
 		} catch (error) {
 			logger.error(error);
 		}
 	},
-	*fetchLlojiBiznesit({payload}) {
+	*fetchArbkGjinia({payload}) {
 		try {
 			const {komunaQuery, vendbanimiQuery, regjioniQuery} = getParams(payload);
 			const response = yield axios.get(
 				// eslint-disable-next-line max-len
-				`/industries/?${komunaQuery}${vendbanimiQuery}${regjioniQuery}&type=llojiBiznesit`,
+				`/industries/?${komunaQuery}${vendbanimiQuery}${regjioniQuery}&type=arbkGjinia`,
 			);
 
-			yield put(actions.fetchLlojiBiznesitSuccess(response?.data));
+			yield put(actions.fetchArbkGjiniaSuccess(response?.data));
 		} catch (error) {
 			logger.error(error);
 		}
 	},
-	*fetchLlojiBiznesitKomuna({payload}) {
+	*fetchArbkStatusi({payload}) {
 		try {
 			const {komunaQuery, vendbanimiQuery, regjioniQuery} = getParams(payload);
 			const response = yield axios.get(
 				// eslint-disable-next-line max-len
-				`/industries/?${komunaQuery}${vendbanimiQuery}${regjioniQuery}&type=llojiBiznesitKomuna`,
+				`/industries/?${komunaQuery}${vendbanimiQuery}${regjioniQuery}&type=arbkStatusi`,
 			);
 
-			yield put(actions.fetchLlojiBiznesitKomunaSuccess(response?.data));
+			yield put(actions.fetchArbkStatusiSuccess(response?.data));
 		} catch (error) {
 			logger.error(error);
 		}
 	},
-	*fetchStatusiBizneseve({payload}) {
+	*fetchArbkKomuna({payload}) {
 		try {
 			const {komunaQuery, vendbanimiQuery, regjioniQuery} = getParams(payload);
 			const response = yield axios.get(
 				// eslint-disable-next-line max-len
-				`/industries/?${komunaQuery}${vendbanimiQuery}${regjioniQuery}&type=statusiBizneseve`,
+				`/industries/?${komunaQuery}${vendbanimiQuery}${regjioniQuery}&type=arbkKomuna`,
 			);
 
-			yield put(actions.fetchStatusiBizneseveSuccess(response?.data));
-		} catch (error) {
-			logger.error(error);
-		}
-	},
-	*fetchSektoreBujqesi({payload}) {
-		try {
-			const {komunaQuery, vendbanimiQuery, regjioniQuery} = getParams(payload);
-			const response = yield axios.get(
-				// eslint-disable-next-line max-len
-				`/industries/?${komunaQuery}${vendbanimiQuery}${regjioniQuery}&type=sektoreBujqesi`,
-			);
-
-			yield put(actions.fetchSektoreBujqesiSuccess(response?.data));
+			yield put(actions.fetchArbkKomunaSuccess(response?.data));
 		} catch (error) {
 			logger.error(error);
 		}
@@ -185,11 +161,10 @@ export const sagas = {
 };
 
 export const watcher = function* w() {
-	yield takeLatest(FETCH_NR_BIZNESEVE, sagas.fetchNrBizneseve);
-	yield takeLatest(FETCH_NR_BIZNESEVE_KOMUNA, sagas.fetchNrBizneseveKomuna);
-	yield takeLatest(FETCH_GJINIA, sagas.fetchGjinia);
-	yield takeLatest(FETCH_LLOJI_BIZNESIT, sagas.fetchLlojiBiznesit);
-	yield takeLatest(FETCH_LLOJI_BIZNESIT_KOMUNA, sagas.fetchLlojiBiznesitKomuna);
-	yield takeLatest(FETCH_STATUSI_BIZNESEVE, sagas.fetchStatusiBizneseve);
-	yield takeLatest(FETCH_SEKTORE_BUJQESI, sagas.fetchSektoreBujqesi);
+	yield takeLatest(FETCH_ARBK_LLOJI_BIZNESIT, sagas.fetchArbkLlojiBiznesit);
+	yield takeLatest(FETCH_ARBK_VITI, sagas.fetchArbkViti);
+	yield takeLatest(FETCH_ARBK_SEKTORI, sagas.fetchArbkSektori);
+	yield takeLatest(FETCH_ARBK_GJINIA, sagas.fetchArbkGjinia);
+	yield takeLatest(FETCH_ARBK_STATUSI, sagas.fetchArbkStatusi);
+	yield takeLatest(FETCH_ARBK_KOMUNA, sagas.fetchArbkKomuna);
 };
