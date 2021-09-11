34aaac5ca0b2cebf4ce6b42c769bc5cf7252f299
Eduanath <eduana.thaci@studentet.uni-pr.edu>
2021-09-11 21:19:09 +0200

    added new graphs for AKK

--- src/components/Graphs/AKK.jsx
+++ src/components/Graphs/AKK.jsx
@@ -1,6 +1,9 @@
 import React, {useEffect} from "react";
 import {connect} from "react-redux";
 import {actions} from "@sagas/industries/akk";
+import Chart from "@common/Chart";
+import {groupBy} from "lodash";
+import {getDatasets, sortLabels} from "./utils";
 
 function AKK({
 	fetchKategoria,
@@ -18,15 +21,118 @@ function AKK({
 	filters,
 }) {
 	useEffect(() => {
-		fetchKategoria();
-		fetchPronesia();
-		fetchKlasa();
-		fetchLlojiNdertesesSiperfaqja();
-		fetchLlojiNdertesesNumri();
-		fetchTipiPronesNumri();
+		fetchKategoria(filters);
+		fetchPronesia(filters);
+		fetchKlasa(filters);
+		fetchLlojiNdertesesSiperfaqja(filters);
+		fetchLlojiNdertesesNumri(filters);
+		fetchTipiPronesNumri(filters);
 	}, [filters]);
 
-	return <div>{/* Graphs here */}</div>;
+	const kategoriaDataSets = getDatasets({
+		filters,
+		items: kategoria,
+		singleItemLabel: "Siperfaqja",
+		property: "siperfaqja",
+		filterBy: "kategoria",
+	});
+
+	const pronesiaDataSets = getDatasets({
+		filters,
+		items: pronesia,
+		singleItemLabel: "Siperfaqja",
+		property: "siperfaqja",
+		filterBy: "pronesia",
+	});
+
+	const klasaDataSets = getDatasets({
+		filters,
+		items: klasa,
+		singleItemLabel: "Siperfaqja",
+		property: "siperfaqja",
+		filterBy: "klasa",
+	});
+
+	const llojiNdertesesSiperfaqjaDataSets = getDatasets({
+		filters,
+		items: llojiNdertesesSiperfaqja,
+		singleItemLabel: "Siperfaqja",
+		property: "siperfaqja",
+		filterBy: "llojinderteses",
+	});
+
+	const llojiNdertesesNumriDataSets = getDatasets({
+		filters,
+		items: llojiNdertesesNumri,
+		singleItemLabel: "Numri i ndertesave",
+		property: "nrndertesave",
+		filterBy: "llojinderteses",
+	});
+
+	const tipiPronesNumriDataSets = getDatasets({
+		filters,
+		items: tipiPronesNumri,
+		singleItemLabel: "Siperfaqja",
+		property: "siperfaqja",
+		filterBy: "tipiprones",
+	});
+
+	return (
+		<>
+			<Chart
+				title="Siperfaqja sipas kategorise"
+				type="bar"
+				data={{
+					labels: sortLabels(Object.keys(groupBy(kategoria, "kategoria"))),
+					datasets: kategoriaDataSets,
+				}}
+			/>
+			<Chart
+				title="Siperfaqja sipas pronesise"
+				type="bar"
+				data={{
+					labels: sortLabels(Object.keys(groupBy(pronesia, "pronesia"))),
+					datasets: pronesiaDataSets,
+				}}
+			/>
+			<Chart
+				title="Siperfaqja sipas klases"
+				type="bar"
+				data={{
+					labels: sortLabels(Object.keys(groupBy(klasa, "klasa"))),
+					datasets: klasaDataSets,
+				}}
+			/>
+			<Chart
+				title="Siperfaqja sipas llojit te nderteses"
+				type="bar"
+				data={{
+					labels: sortLabels(
+						Object.keys(groupBy(llojiNdertesesSiperfaqja, "llojinderteses")),
+					),
+					datasets: llojiNdertesesSiperfaqjaDataSets,
+				}}
+			/>
+			<Chart
+				title="Numri i ndertesave sipas llojit te nderteses"
+				type="bar"
+				data={{
+					labels: sortLabels(Object.keys(groupBy(llojiNdertesesNumri, "llojinderteses"))),
+					datasets: llojiNdertesesNumriDataSets,
+				}}
+			/>
+			<Chart
+				title="Numri i ndertesave sipas tipit te prones"
+				type="pie"
+				data={{
+					labels: sortLabels(Object.keys(groupBy(tipiPronesNumri, "tipiprones")))?.map(
+						(label) => (label === "NULL" ? "N/A" : label),
+					),
+					datasets: tipiPronesNumriDataSets,
+				}}
+			/>
+		</>
+	);
 }
 
 const mapDispatchToProps = {
