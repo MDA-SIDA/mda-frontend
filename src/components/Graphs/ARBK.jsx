2c77482b804daa7265dda5be0a0c3fda9f67260a
Eduanath <eduana.thaci@studentet.uni-pr.edu>
2021-10-05 07:00:49 +0200

    added Loader while fetching ARBK data

--- src/components/Graphs/ARBK.jsx
+++ src/components/Graphs/ARBK.jsx
@@ -4,15 +4,11 @@ import {actions} from "@sagas/industries/arbk";
 import Chart from "@common/Chart";
 import PieChart from "@common/Chart/Pie";
 import {groupBy} from "lodash";
+import Loader from "@common/Loader";
 import {getDatasets, sortLabels, getStatusiBizneseveDataset, getGjiniaDataset} from "./utils";
 
 function ARBK({
-	fetchArbkLlojiBiznesit,
-	fetchArbkViti,
-	fetchArbkSektori,
-	fetchArbkGjinia,
-	fetchArbkStatusi,
-	fetchArbkKomuna,
+	fetchAll,
 	arbkLlojiBiznesit,
 	arbkViti,
 	arbkSektori,
@@ -20,23 +16,11 @@ function ARBK({
 	arbkStatusi,
 	arbkKomuna,
 	filters,
+	isLoading,
 }) {
 	useEffect(() => {
-		fetchArbkLlojiBiznesit(filters);
-		fetchArbkViti(filters);
-		fetchArbkSektori(filters);
-		fetchArbkGjinia(filters);
-		fetchArbkStatusi(filters);
-		fetchArbkKomuna(filters);
-	}, [
-		fetchArbkLlojiBiznesit,
-		fetchArbkViti,
-		fetchArbkSektori,
-		fetchArbkGjinia,
-		fetchArbkStatusi,
-		fetchArbkKomuna,
-		filters,
-	]);
+		fetchAll(filters);
+	}, [fetchAll, filters]);
 
 	const arbkLlojiBiznesiDataSets = getDatasets({
 		filters,
@@ -75,82 +59,86 @@ function ARBK({
 
 	return (
 		<>
-			<Chart
-				title="Numri i bizneseve sipas llojit"
-				type="bar"
-				data={{
-					labels: sortLabels(Object.keys(groupBy(arbkLlojiBiznesit, "llojibiznesit"))),
-					datasets: arbkLlojiBiznesiDataSets,
-				}}
-			/>
-			<Chart
-				title="Numri i bizneseve sipas vitit"
-				type="line"
-				data={{
-					labels: sortLabels(Object.keys(groupBy(arbkViti, "viti"))),
-					datasets: arbkVitiDataSets,
-				}}
-			/>
-			<Chart
-				title="Numri i bizneseve sipas sektoreve"
-				type="bar"
-				data={{
-					labels: sortLabels(Object.keys(groupBy(arbkSektori, "sektori"))),
-					datasets: arbkSektoriDataSets,
-				}}
-			/>
-			<Chart
-				title="Numri i bizneseve sipas komunave"
-				type="bar"
-				data={{
-					labels: sortLabels(Object.keys(groupBy(arbkKomuna, "komunaemri"))),
-					datasets: arbkKomunaDataSets,
-				}}
-			/>
-			{gjiniaData?.map((item, index) => (
-				<PieChart
-					key={`${index} item=index`}
-					title={`Numri i bizneseve sipas gjinise${item.label ? `: ${item.label}` : ""}`}
-					data={{
-						labels: ["F", "M"],
-						datasets: [
-							{
-								label: item.label,
-								data: [item.data?.totalfemra, item.data?.totalmeshkuj],
-								backgroundColor: ["#00517D", "#FCCB11"],
-							},
-						],
-					}}
-				/>
-			))}
-			{statusiBizneseveData?.map((item, index) => (
-				<PieChart
-					key={`${index} item=index`}
-					title={`Statusi i bizneseve${item.label ? `: ${item.label}` : ""}`}
-					type="pie"
-					data={{
-						labels: ["Aktiv", "Jo aktiv"],
-						datasets: [
-							{
-								label: item.label,
-								data: [item.data?.countaktiv, item.data?.countjoaktiv],
-								backgroundColor: ["#00517D", "#FCCB11"],
-							},
-						],
-					}}
-				/>
-			))}
+			{isLoading && <Loader />}
+			{!isLoading && (
+				<>
+					<Chart
+						title="Numri i bizneseve sipas llojit"
+						type="bar"
+						data={{
+							labels: sortLabels(
+								Object.keys(groupBy(arbkLlojiBiznesit, "llojibiznesit")),
+							),
+							datasets: arbkLlojiBiznesiDataSets,
+						}}
+					/>
+					<Chart
+						title="Numri i bizneseve sipas vitit"
+						type="line"
+						data={{
+							labels: sortLabels(Object.keys(groupBy(arbkViti, "viti"))),
+							datasets: arbkVitiDataSets,
+						}}
+					/>
+					<Chart
+						title="Numri i bizneseve sipas sektoreve"
+						type="bar"
+						data={{
+							labels: sortLabels(Object.keys(groupBy(arbkSektori, "sektori"))),
+							datasets: arbkSektoriDataSets,
+						}}
+					/>
+					<Chart
+						title="Numri i bizneseve sipas komunave"
+						type="bar"
+						data={{
+							labels: sortLabels(Object.keys(groupBy(arbkKomuna, "komunaemri"))),
+							datasets: arbkKomunaDataSets,
+						}}
+					/>
+					{gjiniaData?.map((item, index) => (
+						<PieChart
+							key={`${index} item=index`}
+							title={`Numri i bizneseve sipas gjinise${
+								item.label ? `: ${item.label}` : ""
+							}`}
+							data={{
+								labels: ["F", "M"],
+								datasets: [
+									{
+										label: item.label,
+										data: [item.data?.totalfemra, item.data?.totalmeshkuj],
+										backgroundColor: ["#00517D", "#FCCB11"],
+									},
+								],
+							}}
+						/>
+					))}
+					{statusiBizneseveData?.map((item, index) => (
+						<PieChart
+							key={`${index} item=index`}
+							title={`Statusi i bizneseve${item.label ? `: ${item.label}` : ""}`}
+							type="pie"
+							data={{
+								labels: ["Aktiv", "Jo aktiv"],
+								datasets: [
+									{
+										label: item.label,
+										data: [item.data?.countaktiv, item.data?.countjoaktiv],
+										backgroundColor: ["#00517D", "#FCCB11"],
+									},
+								],
+							}}
+						/>
+					))}
+				</>
+			)}
 		</>
 	);
 }
 
 const mapDispatchToProps = {
-	fetchArbkLlojiBiznesit: actions.fetchArbkLlojiBiznesit,
-	fetchArbkViti: actions.fetchArbkViti,
-	fetchArbkSektori: actions.fetchArbkSektori,
-	fetchArbkGjinia: actions.fetchArbkGjinia,
-	fetchArbkStatusi: actions.fetchArbkStatusi,
-	fetchArbkKomuna: actions.fetchArbkKomuna,
+	fetchAll: actions.fetchAll,
 };
 const mapStateToProps = (state) => ({
 	arbkLlojiBiznesit: state.app.industries.arbk.arbkLlojiBiznesit,
@@ -159,6 +147,7 @@ const mapStateToProps = (state) => ({
 	arbkGjinia: state.app.industries.arbk.arbkGjinia,
 	arbkStatusi: state.app.industries.arbk.arbkStatusi,
 	arbkKomuna: state.app.industries.arbk.arbkKomuna,
+	isLoading: state.app.layout.index.loading,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ARBK);
