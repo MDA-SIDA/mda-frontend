a54d3d36eb1e19aa559c6909e060ab8089b772ed
Eduanath <eduana.thaci@studentet.uni-pr.edu>
2021-10-05 07:42:29 +0200

    added Loader while fetching AUV data

--- src/components/Graphs/AUV.jsx
+++ src/components/Graphs/AUV.jsx
@@ -3,22 +3,13 @@ import {connect} from "react-redux";
 import {actions} from "@sagas/industries/auv";
 import {groupBy} from "lodash";
 import Chart from "@common/Chart";
+import Loader from "@common/Loader";
 import {getDatasets, sortLabels} from "./utils";
 
-function AUV({
-	fetchAuvRajoniKomuna,
-	fetchAuvKategoria,
-	fetchAuvKomunaNrKafsheve,
-	auvRajoniKomuna,
-	auvKategoria,
-	auvKomunaNrKafsheve,
-	filters,
-}) {
+function AUV({fetchAll, isLoading, auvRajoniKomuna, auvKategoria, auvKomunaNrKafsheve, filters}) {
 	useEffect(() => {
-		fetchAuvRajoniKomuna(filters);
-		fetchAuvKategoria(filters);
-		fetchAuvKomunaNrKafsheve(filters);
-	}, [filters]);
+		fetchAll(filters);
+	}, [fetchAll, filters]);
 
 	const auvRajoniKomunaDataSets = getDatasets({
 		filters,
@@ -46,35 +37,41 @@ function AUV({
 	});
 	return (
 		<>
-			<Chart
-				title="Numri i fermave"
-				type="bar"
-				data={{
-					labels: sortLabels(Object.keys(groupBy(auvRajoniKomuna, "komunaemri"))),
-					datasets: auvRajoniKomunaDataSets,
-				}}
-			/>
-			<Chart
-				title="Numri i kafsheve"
-				type="bar"
-				data={{
-					labels: sortLabels(Object.keys(groupBy(auvKomunaNrKafsheve, "komunaemri"))),
-					datasets: auvKomunaNrKafsheveDataSets,
-				}}
-			/>
+			{isLoading && <Loader />}
+			{!isLoading && (
+				<>
+					<Chart
+						title="Numri i fermave"
+						type="bar"
+						data={{
+							labels: sortLabels(Object.keys(groupBy(auvRajoniKomuna, "komunaemri"))),
+							datasets: auvRajoniKomunaDataSets,
+						}}
+					/>
+					<Chart
+						title="Numri i kafsheve"
+						type="bar"
+						data={{
+							labels: sortLabels(
+								Object.keys(groupBy(auvKomunaNrKafsheve, "komunaemri")),
+							),
+							datasets: auvKomunaNrKafsheveDataSets,
+						}}
+					/>
+				</>
+			)}
 		</>
 	);
 }
 
 const mapDispatchToProps = {
-	fetchAuvRajoniKomuna: actions.fetchAuvRajoniKomuna,
-	fetchAuvKategoria: actions.fetchAuvKategoria,
-	fetchAuvKomunaNrKafsheve: actions.fetchAuvKomunaNrKafsheve,
+	fetchAll: actions.fetchAll,
 };
 const mapStateToProps = (state) => ({
 	auvRajoniKomuna: state.app.industries.auv.auvRajoniKomuna,
 	auvKategoria: state.app.industries.auv.auvKategoria,
 	auvKomunaNrKafsheve: state.app.industries.auv.auvKomunaNrKafsheve,
+	isLoading: state.app.layout.index.loading,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(AUV);
